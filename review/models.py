e785ee4c5c8c5704b1fb3b716b776430aaa83886
aaghamohammadi <al.aghamohammadi@gmail.com>
2016-01-02 17:35:45 +0330

    ashkan: create model comment+ reating

--- review/models.py
+++ review/models.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from user.models import Customer, Tasker
+
+
+class Comment(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    title = models.CharField("عنوان", default='', max_length=25)
+    context = models.TextField("متن نظر")
+
+
+class Rating(models.Model):
+    tasker = models.ForeignKey(Tasker)
+    customer = models.ForeignKey(Customer)
+    kindness = models.PositiveSmallIntegerField("خوش رویی", default=0)
+    speed = models.PositiveSmallIntegerField("سرعت عمل", default=0)
+    performance = models.PositiveSmallIntegerField("کارایی", default=0)
+    price = models.PositiveSmallIntegerField("قیمت مناسب", default=0)
