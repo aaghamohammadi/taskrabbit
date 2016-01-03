d36d6de1ce2370f0c29be603ac6fd35c2638c5fe
taghal <hamidkazemi22@gmail.com>
2016-01-03 12:04:41 +0330

    da ta az name ha farsi shod

--- manager/forms/edit_task_form.py
+++ manager/forms/edit_task_form.py
@@ -3,15 +3,17 @@ from django import forms
 
 from service.models import TaskModel
 
-
 __author__ = 'garfild'
 
 
 class EditTaskForm(ModelForm):
     name = forms.CharField(required=True, label='نام خدمت', widget=forms.TextInput(
         attrs={'class': 'required'}))
-    image = forms.ImageField(required=True)
+    image = forms.ImageField(required=True, label='عکس')
 
     class Meta:
         model = TaskModel
         fields = ['name', 'image', 'description']
+        labels = {
+            'description': 'شرح خدمت'
+        }
